9a0315cfcfce405f6c140df8b92a41e168930370
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-03-26 03:04:00 -0700

    post endpoint to retrieve random curveball

--- src/game/game-router.js
+++ src/game/game-router.js
@@ -5,14 +5,50 @@ const express =require('express');
 //const bodyParser=express.json();
 const gameService=require('./game-service')
 const gameRouter = express.Router();
+const jsonBodyParser = express.json();
 
 gameRouter
-.get('/',(req,res,next)=>{
-    gameService.getAllInfo(req.app.get('db'))
-    .then(start=>{
-        res.json(gameService.serializedStart(start))
+.route('/')
+    .get((req,res,next)=>{
+        gameService.getAllInfo(req.app.get('db'))
+        .then(start=>{
+            res.json(gameService.serializedStart(start))
+        })
+        .catch(next)
     })
-    .catch(next)
+
+    .post(jsonBodyParser, async (req, res, next) => {
+        let { location } = req.body 
+        if(!location){
+            return res.status(400).json({
+                error: `Missing 'location' in request body`
+            })
+        }
+        let amount = await gameService.getCount(req.app.get('db'), location)
+        amount = amount.rows[0].count
+        let rand = Math.floor(Math.random() * amount) + 1
+        gameService.getRand(req.app.get('db'), location, rand)
+        .then(curveball => {
+            let yesObj = curveball.rows[0]
+            let noObj = curveball.rows[1] 
+            let output = {
+                question: yesObj.question,
+                yes: {
+                    health: yesObj.health,
+                    boredom: yesObj.boredom,
+                    toiletpaper: yesObj.toiletpaper,
+                    food: yesObj.food
+                },
+                no: {
+                    health: noObj.health,
+                    boredom: noObj.boredom,
+                    toiletpaper: noObj.toiletpaper,
+                    food: noObj.food
+                }
+            }
+            res.json(output)
+        })
+        .catch(next)
 })
 
 module.exports = gameRouter;
--- src/game/game-service.js
+++ src/game/game-service.js
@@ -13,6 +13,31 @@ const gameService={
             toiletpaper:info.toiletpaper,
             food:info.food,
         }
+    },
+
+    getCount(knex, location){
+        return knex.raw(`
+        SELECT COUNT(*)
+        FROM curveball
+        WHERE setting = '${location}';`)
+    },
+
+    getRand (knex, location, rowNum) {
+        return knex.raw(`
+        WITH tempTable AS (
+            SELECT 
+                ROW_NUMBER() OVER(
+                     ORDER BY 
+                        setting
+                ) row_num, *
+             FROM 
+                curveball
+             where curveball.setting = '${location}' 
+        )
+        SELECT *
+        FROM tempTable
+        INNER JOIN impact ON tempTable.id = impact.curveball_id
+        AND row_num = ${rowNum};`)
     }
 }
 
